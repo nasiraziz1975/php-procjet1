ba6bfbc3d75452b00e4419d68c5bd7935436a5c0
nasir <nasir_bch1@hotmail.com>
2015-10-29 19:01:55 +0100

    lagt nye filer og testet.

--- index.php
+++ index.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
-    <head>
-        <meta charset="windows-1252">
-        <title></title>
-    </head>
     <body>
-        <?php
-            echo "Hello world!";
-        ?>
+        
+        Welcome <?php echo $_POST["name"]; ?><br>
+        Din ID nummer er :<?php echo $_POST["number"]; ?>
+                
     </body>
 </html>
---
+++ inndata.php
@@ -0,0 +1,36 @@
+<html>
+    <head>
+        
+        
+    </head>
+    <body>
+        <form action="index.php" method="get">
+            Navn: <input type="text" name="name"><br>
+            id nummer : <input type="number"><br>
+            <input type="submit">
+                   
+            
+            
+            
+        </form>
+        
+        
+    </body>
+    
+    
+    
+    
+    
+    
+    
+    
+</html>
+
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
